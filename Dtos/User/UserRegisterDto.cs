82e809d9df30f20bdb3b8250938045f9ea8be1bb
Noah Allen <noahkrallen@gmail.com>
2023-10-07 09:42:02 -0700

    Attempting to get program to accept there is a User folder under the Dtos folder

---
+++ Dtos/User/UserRegisterDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace dotnet_rpg.Dtos
+{
+	public class UserRegisterDto
+	{
+        public string Username { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+    }
+}
